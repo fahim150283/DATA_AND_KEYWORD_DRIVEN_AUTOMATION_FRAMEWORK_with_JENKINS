53abea301c8ac8b38ceff174791ecc50b619a10f
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-06-29 15:38:28 +0600

    the custom softassertion is being implemented

--- src/test/java/TestCases/BankManagerLoginTest.java
+++ src/test/java/TestCases/BankManagerLoginTest.java
@@ -4,8 +4,10 @@ import Base.TestBase;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class BankManagerLoginTest extends TestBase {
+    utilities.CustomSoftAssert softAssert = new utilities.CustomSoftAssert(driver, test);
 
     @Test(priority = 1)
     public void loginTest() {
@@ -21,7 +23,9 @@ public class BankManagerLoginTest extends TestBase {
     @Test
     public void FailAssertionTest() {
         logger.info("checking add customer button");
-        Assert.assertTrue(isElementPresent(By.cssSelector("buttonm")), "Add customer button is not present");
+        softAssert.assertTrue(isElementPresent(By.cssSelector("buttonm")), "Add customer button is not present");
+        softAssert.assertEquals(driver.findElement(By.xpath("//strong[@class='mainHeading']")).getText(), "halum", "Add customer button is not present");
         logger.info("checking add customer button completed");
+        softAssert.assertAll();
     }
 }
---
+++ src/test/java/Utilities/CustomSoftAssert.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import Utilities.ScreenshotUtil;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomSoftAssert {
+    private SoftAssert softAssert;
+    private WebDriver driver;
+    private com.relevantcodes.extentreports.ExtentTest test;
+    private List<String> failureScreenshots;
+
+    public CustomSoftAssert(WebDriver driver, com.relevantcodes.extentreports.ExtentTest test) {
+        this.softAssert = new SoftAssert();
+        this.driver = driver;
+        this.test = test;
+        this.failureScreenshots = new ArrayList<>();
+    }
+
+    public void assertTrue(boolean condition, String message) {
+        try {
+            softAssert.assertTrue(condition, message);
+            test.log(LogStatus.PASS, message);
+        } catch (AssertionError e) {
+            String screenshotPath = ScreenshotUtil.captureScreenshot(driver, message + ".png");
+            test.log(LogStatus.FAIL, message + test.addScreenCapture(screenshotPath));
+            failureScreenshots.add(screenshotPath);
+        }
+    }
+
+    public void assertEquals(Object actual, Object expected, String message) {
+        try {
+            softAssert.assertEquals(actual, expected, message);
+            test.log(LogStatus.PASS, message);
+        } catch (AssertionError e) {
+            String screenshotPath = ScreenshotUtil.captureScreenshot(driver, message + ".png");
+            test.log(LogStatus.FAIL, message + " | Expected: " + expected + " but got: " + actual + test.addScreenCapture(screenshotPath));
+            failureScreenshots.add(screenshotPath);
+        }
+    }
+
+    public void assertAll() {
+        softAssert.assertAll();
+    }
+}
